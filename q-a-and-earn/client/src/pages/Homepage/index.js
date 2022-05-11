c0edfdd941d1221f4c278ed615ebb93be0233079
JasonxWarren <warrenjason1@gmail.com>
2022-05-11 19:07:17 -0400

    auth progress pt1

--- q-a-and-earn/client/src/pages/Homepage/index.js
+++ q-a-and-earn/client/src/pages/Homepage/index.js
@@ -1,89 +1,79 @@
-import {Route, Routes} from "react-router-dom";
+
+import { Route, Routes} from "react-router-dom";
+import { useReducer,useEffect } from "react";
+import * as authService from "../../api/auth.service";
+import Welcome from "../../components/Start";
+import NavBar from "../../components/NavBar";
 import UserIndex from "../../components/UserProfile";
 
+const reducer = (prevState, action) => {
+    switch(action.type) {
+        case 'setIsLoggedIn':
+            return {...prevState, isLoggedIn: action.payload};
+        default:
+            return prevState
+    }
+}
+
+const initialState = {
+    isLoggedIn: false,
+}
+
 const Home = () => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const { isLoggedIn } = state;
+
+    const userActive = () => {
+        if (authService.currentUser()) {
+            dispatch({type: 'setIsLoggedIn', payload: true})
+        }else{
+            dispatch({type: 'setIsLoggedIn', payload: false})
+        }
+    }
+
+    useEffect(() => {
+        userActive();
+    }, []);
+
+    if (isLoggedIn) {
+
     return (
         <>
-         <Routes> 
-             <Route  
-            path='/'
-            element={<UserIndex/>}
-            /> 
-         </Routes> 
-        </>
-    )}
-    export default Home;
 
-
-
-
-// import { Route, Routes} from "react-router-dom";
-// import { useReducer,useEffect } from "react";
-// import * as authService from "../../api/auth.service";
-// import Welcome from "../../components/Start";
-// import NavBar from "../../components/NavBar";
-// import UserIndex from "../../components/UserProfile";
-
-// const reducer = (prevState, action) => {
-//     switch(action.type) {
-//         case 'setIsLoggedIn':
-//             return {...prevState, isLoggedIn: action.payload};
-//         default:
-//             return prevState
-//     }
-// }
-
-// const initialState = {
-//     isLoggedIn: false,
-// }
-
-// const Home = () => {
-//     const [state, dispatch] = useReducer(reducer, initialState);
-//     const { isLoggedIn } = state;
-
-//     const userActive = () => {
-//         if (authService.currentUser()) {
-//             dispatch({type: 'setIsLoggedIn', payload: true})
-//         }else{
-//             dispatch({type: 'setIsLoggedIn', payload: false})
-//         }
-//     }
-
-//     useEffect(() => {
-//         userActive();
-//     }, []);
-
-//     if (isLoggedIn) {
-
-//     return (
-//         <>
-
-//         <div>
+        <div>
     
-//   <NavBar 
-//               checkUserActive={() => userActive()}
-//           />
-//         </div>
-//         { <h1>Successful Sign In</h1> }
+  <NavBar 
+              checkUserActive={() => userActive()}
+          />
+        </div>
+        { <h1>Successful Sign In</h1> }
 
-//             <Routes>
-//                 <Route  
-//                     path='/'
-//                     element={<UserIndex/>}
-//                     />
-//             </Routes>
-//         </>
-//     )
-//     } else {
-//         return (
-//             <div>
-//              	<div>
-// 				        <Welcome checkUserActive={() => dispatch({type: "setIsLoggedIn", payload: true})}/>
-// 			        </div>
-//             </div>
-//         )
-//     }
+            <Routes>
+                <Route  
+                    path='/'
+                    element={<UserIndex/>}
+                    />
+                    
+            </Routes>
+        </>
+    )
+    } else {
+        return (
+            <div>
+             	<div>
+				        <Welcome checkUserActive={() => dispatch({type: "setIsLoggedIn", payload: true})}/>
+                        <Routes>
+                            <Route  
+                                path='register'
+                                element={<UserIndex/>}
+                            />
+                    
+                        </Routes>
+			        </div>
+            </div>
+        )
+    }
 
-// }
+}
 
-// export default Home;
+export default Home;
--- q-a-and-earn/server/routes/index.js
+++ q-a-and-earn/server/routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.use('/user', require('./user'));
-router.use('/question', require("./question"));
+// router.use('/question', require("./question"));
 router.use("/auth", require("./auth"));
 
 module.exports = router;
