8e5f531a08bb3cb5880e2cf854b556d0a4e6c51d
JasonxWarren <warrenjason1@gmail.com>
2022-05-11 22:14:46 -0400

    updated data shown for user profile

--- q-a-and-earn/client/src/components/UserProfile/index.js
+++ q-a-and-earn/client/src/components/UserProfile/index.js
@@ -4,13 +4,16 @@ import * as userProfileService from "../../api/userprofile.service";
 const UserIndex = () => {
     const [description, setDescription]= useState("");
     const [username, setUsername]= useState("");
-    
-
+    const [walletBalance, setWalletBalance]= useState("");
+    const [expertise, setExpertise]= useState("");
     const getExistingProfile =  () => {
         let res =  userProfileService.show()
             .then((data) => {
              console.log("get existing profile: ", data.data.data)
             setUsername(data.data.data.username);
+            setDescription(data.data.data.description)
+            setWalletBalance(data.data.data.walletBalance)
+            setExpertise(data.data.data.expertise[0])
             // setUser(data.data.description);
         });
         if ( !res === 201 ) {
@@ -24,7 +27,10 @@ const UserIndex = () => {
 return (    
     <div>
                          <h3>Welcome</h3>
+                            <h1>{walletBalance}</h1>
                             <h2>{username}</h2>
+                            <h3>{description}</h3>
+                            <button>{expertise}</button>
                            
                 </div>
 );  
