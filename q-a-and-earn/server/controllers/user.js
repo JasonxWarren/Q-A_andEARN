77e0c4a575ce58a0d9841e28b92b7157b2c14ebb
JasonxWarren <warrenjason1@gmail.com>
2022-05-11 22:42:51 -0400

    added initial  update controller

--- q-a-and-earn/server/controllers/user.js
+++ q-a-and-earn/server/controllers/user.js
@@ -56,10 +56,49 @@ const show= (req,res) => {
 })
 }
 
+//Update profile 
+const updateProfile= (req, res) => {
+    // console.log("req.body: ", req.body)
+    // console.log("userId: ", req.userId)
+    db.User.findByIdAndUpdate(
+        {_id: req.userId },
+        {
+            $set: {
+                description: req.body.description,
+                expertise: req.body.expertise,  
+            }
+        }, 
+        {new: true},
+        (err,foundProfile) => {
+            // console.log("found profile line 55: ",foundProfile)
+        if (err) {
+            return res.status(400)
+            .json({
+                message: "Failed to edit the profile.",
+                error: err,
+            })
+        } 
+        //else {
+        //     console.log(foundProfile);
+        //     foundProfile[0].push(req.userId.username)
+        //     foundProfile[0].save();
+        // }
+         else {
+            // console.log("found profile line 69: "+foundProfile)
+            return res.status(200).json({
+                message: "Updated User Profile",
+                data: foundProfile,
+                id: req.userId
+        })
+    }
+    })
+}
+
 
 
 module.exports = {
     index,
     show,
+    updateProfile,
 }
 
